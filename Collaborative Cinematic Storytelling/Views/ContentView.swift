e2f9a1649a3c53aef6073a23455c26b8700d9db4
Hasan Tahir <hasantahir24011997@gmail.com>
2022-05-10 13:17:52 +0500

    ipad mini adaptation

--- Collaborative Cinematic Storytelling/Views/ContentView.swift
+++ Collaborative Cinematic Storytelling/Views/ContentView.swift
@@ -9,8 +9,8 @@ import SwiftUI
 import CoreData
 
 
-let cardHeight : CGFloat = 110
-let cardWidth : CGFloat = 154
+let cardHeight : CGFloat = UIScreen.main.bounds.width * 0.09
+let cardWidth : CGFloat = UIScreen.main.bounds.width * 0.125
 
 struct ContentView: View {
 
